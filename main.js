1ceab09d93c1c86385a07ae3b8b348fc6c759e9a
literate <gaekoon@gmail.com>
2022-06-26 17:35:57 +0900

    의존성 버전 연습(^, ~)

--- main.js
+++ main.js
@@ -1,14 +1,18 @@
-const decamelized = (text, separator = '_') => {
+const decamelize = (text, separator = '_') => {
   const replacement = `$1${separator}$2`;
-  return text.replace(
-    /([\p{Lowercase_Letter}\d])(\p{Uppercase_Letter})/gu,
-    replacement
-  );
+  return text
+    .replace(/([\p{Lowercase_Letter}\d])(\p{Uppercase_Letter})/gu, replacement)
+    .toLowerCase();
 };
 
-const result = decamelized('rainbowUnicorn');
+const result = decamelize('javaScript');
 console.log(result);
 
 /**
  * 정규표현식에서 각 capture group을 $1, $2로 표현
  */
+
+var re = /(\w+)\s(\w+)/;
+var str = 'John Smith';
+const strr = str.replace(re, '$2, $1'); // "Smith, John"
+console.log(strr);
